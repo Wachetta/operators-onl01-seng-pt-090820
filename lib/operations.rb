42208755910d8846df1525746d462576dba333fe
Zachary Darneille <zakdarn@gmail.com>
2020-09-10 20:45:59 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,27 +1,7 @@
 def unsafe?(speed)
 if speed > 60
   true
-elsif speed == 40
-  false
-elsif speed == 41
-  false
-elsif speed == 42
-false
-elsif speed == 43
-  false
-elsif speed == 44
-  false
-elsif speed == 45
-  false
-elsif speed == 46
-  false
-elsif speed == 47
-  false
-elsif speed == 48
-  false
-elsif speed == 49
-  false
-elsif speed == 50
+elsif speed < 40 || speed < 60
   false
 else speed < 40
   true
